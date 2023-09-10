10da239d614da7d15a9a4ebed6faa5c593e4c3ac
saic2 <saic2@192.168.1.5>
2023-09-10 22:41:20 +0530

    first commit
    upload all files

---
+++ DAY1/src/com/svit/dayone/datatypes/CharDemo.java
@@ -0,0 +1,23 @@
+package com.svit.dayone.datatypes;
+
+public class CharDemo {
+	public static void main(String[] args) {
+		char ch ='A';
+		System.out.println(ch);
+		char ch1=65;
+		System.out.println(ch1);
+		char ch2=66;
+		System.out.println(ch2);
+		char var1 ='\u00A7';
+		System.out.println(var1);
+		char var2 ='\u20ac';
+		System.out.println(var2);
+		char var3='\u221e';
+		System.out.println(var3);
+		int a='A';
+		System.out.println(a);
+		char a1='A';
+		System.out.println(a1);
+		
+	}
+}
---
+++ DAY1/src/com/svit/dayone/datatypes/DataTypesDemo.java
@@ -0,0 +1,17 @@
+package com.svit.dayone.datatypes;
+
+public class DataTypesDemo {
+	public static void main (String[] args) {
+		int v1=101/61;
+		float v2=101/61;
+		double v3=10d/6d;
+		float v4=5/2.5f;
+		int markes =512;
+		System.out.println("Value1="+" "+v1);
+		System.out.println("Value2= "+v2);
+		System.out.println("Value3= "+v3);
+		System.out.println("Value4= "+v4);
+		double percentage=markes*0.46f;
+		System.out.println("Percentage: "+percentage);
+	}
+}
